32c9298a4718dc37d32f2d3dd1a96646a98cdbf2
Adam <adamlenter1@gmail.com>
2022-04-22 20:45:36 -0400

    Colleect all of a user's carpools - as driver and passeenger - in the user get fetch.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,9 @@ class User < ActiveRecord::Base
     has_many :bank_accounts
     belongs_to :city 
     belongs_to :home_neighborhood_location, :class_name => 'Location'
+    has_many :carpools_as_driver, :foreign_key => 'driver_user_id', :class_name => 'Carpool'
     has_many :carpool_guests
     has_many :carpools_as_guest, :through => :carpool_guests, :source => 'carpool'
-    has_many :carpools
-    has_many :user_transactions, :class_name => 'Sender_User'
+    has_many :user_transactions, :foreign_key => 'sender_user_id'
+    has_many :user_transactions, :foreign_key => 'recipient_user_id'
 end
--- app/models/user_transaction.rb
+++ app/models/user_transaction.rb
@@ -2,5 +2,6 @@ class UserTransaction < ActiveRecord::Base
     belongs_to :carpool_guest
     belongs_to :bank_account
     belongs_to :sender_user, :class_name => 'User'
+    belongs_to :recipient_user, :class_name => 'User'
     belongs_to :carpool
 end
